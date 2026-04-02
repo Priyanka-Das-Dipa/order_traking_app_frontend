f1c16c5da122321b6cfc6d5313a7bfa62098ce91
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2026-04-03 00:03:37 +0600

    create fake data

---
+++ src/utilities/menuData.js
@@ -0,0 +1,100 @@
+export const menuItems = [
+  {
+    id: 'item-001',
+    name: 'Margherita Pizza',
+    description: 'Classic tomato sauce, mozzarella, and fresh basil',
+    price: 12.99,
+    category: 'Pizza',
+    image: 'https://ibb.co/zTmWDvbT'
+  },
+  {
+    id: 'item-002',
+    name: 'Pepperoni Pizza',
+    description: 'Tomato sauce, mozzarella, and pepperoni',
+    price: 14.99,
+    category: 'Pizza',
+    image: 'https://ibb.co/zTmWDvbT'
+  },
+  {
+    id: 'item-003',
+    name: 'Cheese Burger',
+    description: 'Beef patty, cheese, lettuce, tomato, and special sauce',
+    price: 10.99,
+    category: 'Burger',
+    image: 'https://ibb.co/zTmWDvbT'
+  },
+  {
+    id: 'item-004',
+    name: 'Chicken Burger',
+    description: 'Grilled chicken, lettuce, tomato, and mayo',
+    price: 9.99,
+    category: 'Burger',
+    image: 'https://ibb.co/zTmWDvbT'
+  },
+  {
+    id: 'item-005',
+    name: 'Spaghetti Carbonara',
+    description: 'Creamy pasta with bacon and parmesan',
+    price: 13.99,
+    category: 'Pasta',
+    image: 'https://ibb.co/zTmWDvbT'
+  },
+  {
+    id: 'item-006',
+    name: 'Penne Arrabiata',
+    description: 'Spicy tomato sauce with garlic and chili',
+    price: 11.99,
+    category: 'Pasta',
+    image: 'https://ibb.co/zTmWDvbT'
+  },
+  {
+    id: 'item-007',
+    name: 'Caesar Salad',
+    description: 'Romaine lettuce, croutons, parmesan, Caesar dressing',
+    price: 8.99,
+    category: 'Salad',
+    image: 'https://ibb.co/zTmWDvbT'
+  },
+  {
+    id: 'item-008',
+    name: 'Greek Salad',
+    description: 'Tomatoes, cucumber, olives, feta cheese',
+    price: 9.99,
+    category: 'Salad',
+    image: 'https://ibb.co/zTmWDvbT'
+  },
+  {
+    id: 'item-009',
+    name: 'Coca Cola',
+    description: 'Chilled soft drink',
+    price: 2.99,
+    category: 'Drinks',
+    image: 'https://ibb.co/zTmWDvbT'
+  },
+  {
+    id: 'item-010',
+    name: 'Lemonade',
+    description: 'Fresh homemade lemonade',
+    price: 3.99,
+    category: 'Drinks',
+    image: 'https://ibb.co/zTmWDvbT'
+  },
+  {
+    id: 'item-011',
+    name: 'Chocolate Cake',
+    description: 'Rich chocolate cake with chocolate frosting',
+    price: 6.99,
+    category: 'Dessert',
+    image: 'https://ibb.co/zTmWDvbT'
+  },
+  {
+    id: 'item-012',
+    name: 'Ice Cream',
+    description: 'Vanilla ice cream with chocolate sauce',
+    price: 5.99,
+    category: 'Dessert',
+    image: 'https://ibb.co/zTmWDvbT'
+  }
+];
+
+export const categories = ['All', 'Pizza', 'Burger', 'Pasta', 'Salad', 'Drinks', 'Dessert'];
